33ecef1fdf975a2ac2ce2d2aab608318d7ec78e8
Abel Meseret <ameseret1990@gmail.com>
2024-06-18 06:05:16 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const app = express()
 const port = 3000
 
+//Database
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 //Index
 app.get('/players/', (req, res) => {
     res.send("hello world")
 })
+
+// New
+
+
+// Create
+
+
+// Show
+
+
+// Edit
+
+
+// Update
+
+
+// Delete
 
 app.listen(port, () => {
     console.log('listenening on port', port)
