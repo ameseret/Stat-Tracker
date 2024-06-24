5e718c2bd7174baa08c937b3b706236f2bfa1102
Abel Meseret <ameseret1990@gmail.com>
2024-06-24 05:02:04 +0300

    edit playerSchema, index.ejs and new.ejs

--- models/players.js
+++ models/players.js
@@ -4,7 +4,19 @@ const playerSchema = new mongoose.Schema({
     name: { type: String, required: true },
     team: { type: String, required: true },
     position: { type: String, required: true },
-    age: { type: Number, required: true }
+    age: { type: Number, required: true },
+    stats: {
+        points: { type: Number, required: true },
+        rebounds: { type: Number, required: true },
+        assists: { type: Number, required: true },
+        blocks: { type: Number, required: true },
+        steals: { type: Number, required: true },
+        turnovers: { type: Number, required: true },
+        minutesPlayed: { type: Number, required: true },
+        fieldGoalPercentage: { type: Number, required: true },
+        threePointPercentage: { type: Number, required: true },
+        freeThrowPercentage: { type: Number, required: true }
+    }
 });
 
 const Player = mongoose.model('Player', playerSchema);
